6fd0996800aef382da7212162c3fb28a437068c4
郭双鹏 <guoshuangpeng@le.com>
2019-05-12 20:13:32 +0800

    [TABLE]: 数据表创建

--- models/table.go
+++ models/table.go
@@ -17,7 +17,7 @@ const (
 
 /* 数据库表注册 */
 func RegisterModel() {
-	orm.RegisterModel(new(User))
+	orm.RegisterModel(new(User), new(Lab), new(Notice), new(Course), new(Record))
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -37,6 +37,86 @@ type User struct {
 	Name       string    // 昵称
 	Password   string    // 密码
 	Type       int       // 用户类别
+	CreateTime time.Time // 创建时间
+	CreateId   int64     // 创建人
+	UpdateTime time.Time // 更新时间
+	UpdateId   int64     // 修改人
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// 实验室信息表
+// @表名: lab
+// @描述: 存储实验室基本信息
+type Lab struct {
+	Id         int64     // Id
+	Name       string    // 名称
+	Location   string    // 地址
+	CreateTime time.Time // 创建时间
+	CreateId   int64     // 创建人
+	UpdateTime time.Time // 更新时间
+	UpdateId   int64     // 修改人
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// 通知信息表
+// @表名: notice
+// @描述: 存储通知信息
+type Notice struct {
+	Id          int64     // Id
+	Title       string    // 标题
+	Description string    // 描述信息
+	CreateTime  time.Time // 创建时间
+	CreateId    int64     // 创建人
+	UpdateTime  time.Time // 更新时间
+	UpdateId    int64     // 修改人
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// 课程信息表
+
+const (
+	COURSE_STATUS_RESERVING = 1 // 预约中
+	COURSE_STATUS_SUCC      = 2 // 预约成功
+	COURSE_STATUS_FAIL      = 3 // 预约失败
+	COURSE_STATUS_CANCEL    = 4 // 取消
+)
+
+// @表名: course
+// @描述: 存储课程信息
+type Course struct {
+	Id         int64     // Id
+	Name       string    // 课程名称
+	UserId     int64     // 教师ID
+	LabId      int64     // 实验室ID
+	StartTime  time.Time // 开始时间
+	EndTime    time.Time // 结束时间
+	Total      int       // 课程容纳人数
+	Count      int       // 课程当前人数
+	Status     int       // 预约状态
+	CreateTime time.Time // 创建时间
+	CreateId   int64     // 创建人
+	UpdateTime time.Time // 更新时间
+	UpdateId   int64     // 修改人
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// 学生课堂记录信息表
+
+const (
+	RECORD_STATUS_RESERVING = 1 // 预约中
+	RECORD_STATUS_SUCC      = 2 // 预约成功
+	RECORD_STATUS_FAIL      = 3 // 预约失败
+	RECORD_STATUS_CANCEL    = 4 // 取消
+)
+
+// @表名: record
+// @描述: 存储学生课堂记录信息信息
+type Record struct {
+	Id         int64     // Id
+	UserId     int64     // 学生ID
+	CourseId   int64     // 课程ID
+	Status     int       // 预约状态
+	Score      float64   // 得分
 	CreateTime time.Time // 创建时间
 	CreateId   int64     // 创建人
 	UpdateTime time.Time // 更新时间
