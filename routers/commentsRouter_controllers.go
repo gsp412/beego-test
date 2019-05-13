fe725d4d8de05099608144da77cc3d8d156f958a
郭双鹏 <guoshuangpeng@le.com>
2019-05-13 14:33:30 +0800

    [SWAGGER]: 添加swagger文档

---
+++ routers/commentsRouter_controllers.go
@@ -0,0 +1,19 @@
+package routers
+
+import (
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context/param"
+)
+
+func init() {
+
+    beego.GlobalControllerRouter["beego-test/controllers:TokenController"] = append(beego.GlobalControllerRouter["beego-test/controllers:TokenController"],
+        beego.ControllerComments{
+            Method: "Get",
+            Router: `/`,
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+}
